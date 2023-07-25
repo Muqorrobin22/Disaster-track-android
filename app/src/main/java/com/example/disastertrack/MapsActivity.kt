3124cf0ed34afe5b8a9b6313c8c70f8c2c396420
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-07-26 02:32:26 +0700

    MODAL_BOTTOM_SHEET - hide action bar and give the full screen modal

--- app/src/main/java/com/example/disastertrack/MapsActivity.kt
+++ app/src/main/java/com/example/disastertrack/MapsActivity.kt
@@ -6,6 +6,7 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import androidx.core.view.WindowCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -45,6 +46,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             peekHeight = 400
             this.state = BottomSheetBehavior.STATE_COLLAPSED
         }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Hide Action Bar
+        supportActionBar?.hide()
 
     }
 
