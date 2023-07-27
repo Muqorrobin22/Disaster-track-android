6e07a06c22797f433c8b88b7313b65f2e8742773
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-07-27 11:17:52 +0700

    API - create services for reportsAPI

---
+++ app/src/main/java/com/example/disastertrack/model/service/ReportApiService.kt
@@ -0,0 +1,19 @@
+package com.example.disastertrack.model.service
+
+import com.example.disastertrack.model.data.ReportsData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ReportApiService {
+    @GET("reports/archive")
+    fun getReportByYearStartToEnd(
+        @Query("start") start: String,
+        @Query("end") end: String
+    ) : Call<ReportsData>
+
+    @GET("reports")
+    fun getReportByTimeperiod(
+        @Query("timeperiod") timePeriod: Int,
+    ) : Call<ReportsData>
+}
