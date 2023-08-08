db49265a1c6ac39e3d4b9674bc3864d7c20dcf49
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-08-08 16:59:17 +0700

    LOGIC - bugs done when pick date in different year

--- app/src/main/java/com/example/disastertrack/view/MapsActivity.kt
+++ app/src/main/java/com/example/disastertrack/view/MapsActivity.kt
@@ -211,10 +211,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         }
     }
 
+    fun containsTwoCommas(date: String): Int {
+        val parts = date.split(",")
+        return parts.size - 1
+    }
+
+    fun getLastYear(date: String): String {
+        val parts = date.split(", ", " – ")
+        return parts.last()
+    }
+
     fun convertDateRange(dateRange: String): String {
         val dateParts = dateRange.split(" – ")
-        val startDate = parseDate(dateParts[0])
-        val endDate = parseEndDate(dateParts[1])
+//        val startDate = parseDate(dateParts[0])
+//        val endDate = parseEndDate(dateParts[1])
+        lateinit var startDate: Date
+        lateinit var endDate : Date
+
+        if (containsTwoCommas(dateRange) == 1) {
+            Log.d("MainDate", "1 comma")
+            var getLastYear = getLastYear(dateRange)
+            startDate = parseStartDateWithSameYear(dateParts[0], getLastYear.toInt())
+            endDate = parseEndDate(dateParts[1])
+        } else {
+            startDate = parseDate(dateParts[0])
+            endDate = parseEndDate(dateParts[1])
+        }
 
         val convertedStartDate = SimpleDateFormat("yyyy-MM-dd").format(startDate)
         val convertedEndDate = SimpleDateFormat("yyyy-MM-dd").format(endDate)
@@ -234,6 +256,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             val calendar = Calendar.getInstance()
             calendar.time = dateWithoutYear
             calendar.set(Calendar.YEAR, currentYear)
+
+            calendar.time
+        }
+    }
+
+    fun parseStartDateWithSameYear(dateString: String, year : Int): Date {
+        val dateFormat = if (dateString.contains(",")) "MMM d, yyyy" else "MMM d"
+//         SimpleDateFormat(dateFormat, Locale.ENGLISH).parse(dateString)
+
+        return if ( dateString.contains(",")) {
+            SimpleDateFormat(dateFormat, Locale.ENGLISH).parse(dateString)
+        } else {
+            val dateWithoutYear = SimpleDateFormat(dateFormat, Locale.ENGLISH).parse(dateString)
+            val calendar = Calendar.getInstance()
+            calendar.time = dateWithoutYear
+            calendar.set(Calendar.YEAR, year)
 
             calendar.time
         }
